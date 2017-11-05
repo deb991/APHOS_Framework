1a184b8e8b9ecd7b40fd0ea77a19bf0f688d9d73
Ozzius <deb991@users.noreply.github.com>
2017-11-06 02:47:10 +0530

    File created with Minimal description.
    File created with Minimal description. Hence there is only import statement provided.

---
+++ WebFramework/SLTester.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import os
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+__auth__ = "JsOzz"
+
+#Practical on Selenium testing upon Firefox WEB driver due to complexity of Chrome web driver. 
+
